14ca5eab79bc78e17ebf0c28c924d3c7f0e84394
MousteeM <moustee01@gmail.com>
2023-05-02 08:46:54 +0100

    render country data

--- js/app.js
+++ js/app.js
@@ -1,33 +1,45 @@
-const getCountries = async function () {
+const getCountryData = async function () {
   const res = await fetch("https://restcountries.com/v3.1/all");
   const data = await res.json();
-  //   const test = JSON.stringify(data);
-  for (let i = 0; i < 1; i++) {
-    console.log(data[i].independent);
+  console.log(data);
+  for (let i = 0; i < 12; i++) {
+    console.log(data[i].flags.png);
+    renderCountries(data, i);
+    // console.log(data[i].region);
   }
-  console.log(data.length);
+  //   console.log(typeof data);
 };
 
 const cardContainer = document.querySelector(".card-container");
 console.log(cardContainer);
 
-const renderCountries = function (data) {
+function renderCountries(data, id) {
+  const flag = data[id].flags.png;
+  const { common: name } = data[id].name;
+  const population = data[id].population;
+  let capitalArr = data[id].capital;
+  const region = data[id].region;
+  let capital = "";
+  if (!capitalArr) {
+    capital = `No capital available`;
+  } else capital = capitalArr[0];
+
   const html = `
         <div class="card">
             <img
-            src="./assets/fachry-zella-devandra-CHfPNUnCezY-unsplash.jpg"
+            src="${flag}"
             alt=""
             />
             <div class="card-text">
-                <h3 class="country-name">Country name</h3>
-                <p>Population:</p>
-                <p>Region:</p>
-                <p>Capital:</p>
+                <h3 class="country-name">${name}</h3>
+                <p>Population: ${population}</p>
+                <p>Region: ${region}</p>
+                <p>Capital: ${population}</p>
             </div>
         </div>
     `;
 
   cardContainer.insertAdjacentHTML("beforeend", html);
-};
+}
 
-getCountries();
+getCountryData();
