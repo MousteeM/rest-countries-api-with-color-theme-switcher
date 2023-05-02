0fc42783e9bc8a372f548a8e54ab32a2f8199375
MousteeM <moustee01@gmail.com>
2023-05-02 18:38:40 +0100

    fix filter function

--- js/app.js
+++ js/app.js
@@ -1,25 +1,11 @@
 const getCountryData = async function () {
   const res = await fetch("https://restcountries.com/v3.1/all");
   const data = await res.json();
-
+  //   console.log(data);
   for (let i = 0; i < data.length; i++) {
     renderCountries(data, i);
   }
-
-  const searchInput = document.querySelector(".search-input");
-  searchInput.addEventListener("keyup", (e) => {
-    const searchTerm = e.target.value.toLowerCase().trim();
-    cards.forEach((card) => {
-      const countryName = card
-        .querySelector(".country-name")
-        .textContent.toLowerCase();
-      if (!countryName.includes(searchTerm)) {
-        card.style.display = "none";
-      } else {
-        card.style.display = "block";
-      }
-    });
-  });
+  searchCountry();
 };
 
 const cardContainer = document.querySelector(".card-container");
@@ -37,30 +23,26 @@ function renderCountries(data, id) {
   } else capital = capitalArr[0];
 
   const html = `
-        <div class="card">
-            <img
-            src="${flag}"
-            alt=""
-            />
-            <div class="card-text">
-                <h3 class="country-name">${name}</h3>
-                <p><b>Population:</b> ${population}</p>
-                <p><b>Region:</b> ${region}</p>
-                <p><b>Capital:</b> ${capital}</p>
-            </div>
-        </div>
-    `;
+          <div class="card">
+              <img
+              src="${flag}"
+              alt=""
+              />
+              <div class="card-text">
+                  <h3 class="country-name">${name}</h3>
+                  <p><b>Population:</b> ${population}</p>
+                  <p><b>Region:</b> ${region}</p>
+                  <p><b>Capital:</b> ${capital}</p>
+              </div>
+          </div>
+      `;
 
   cardContainer.insertAdjacentHTML("beforeend", html);
 }
 
-// const searchInput = document.querySelector(".search-input");
-const cards = document.querySelectorAll(".card");
-// const cardList = card.getElementsByTagName("h3");
-const countryList = cardContainer.getElementsByTagName("h3");
-
-function searchCountries() {
-  //   const searchInput = document.querySelector(".search-input");
+function searchCountry() {
+  const searchInput = document.querySelector(".search-input");
+  const cards = document.querySelectorAll(".card");
   searchInput.addEventListener("keyup", (e) => {
     const searchTerm = e.target.value.toLowerCase().trim();
     //   console.log(searchTerm);
@@ -73,7 +55,7 @@ function searchCountries() {
         card.style.display = "none";
       } else {
         card.style.display = "block";
-        console.log("match");
+        // console.log("match");
       }
       // console.log(countryName);
     });
